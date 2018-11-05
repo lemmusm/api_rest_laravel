89466039cd4d93edb8484da5611ac02bd0f02f5b
Manuel Hdez. Lemus <lemmusm@gmail.com>
2018-11-05 14:47:53 -0600

    API routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// ROUTES FOR USERS TABLE
+    Route::apiResource('users', 'UserController');
+// ROUTES FOR DPTOS TABLE
+    Route::apiResource('dptos', 'DptoController');
+// ROUTES FOR TICKETS TABLE
+    Route::apiResource('tickets', 'TicketController');
